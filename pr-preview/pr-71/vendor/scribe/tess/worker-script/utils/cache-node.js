2035c4fc1ffb666489824cd032180da4355c2f71
milyin <milyin@users.noreply.github.com>
2026-09-22 15:48:17 +0000

    Remove preview for PR 71 🛬

--- pr-preview/pr-71/vendor/scribe/tess/worker-script/utils/cache-node.js
+++
@@ -1,9 +0,0 @@
-import util from 'util';
-import fs from 'fs';
-
-export const readCache = util.promisify(fs.readFile);
-export const writeCache = util.promisify(fs.writeFile);
-export const deleteCache = (path) => (
-  util.promisify(fs.unlink)(path)
-    .catch(() => {})
-);
